8de9d9a5ce1a0c66eba03b749e1f7873b1628f82
walderf <76556660+walderf@users.noreply.github.com>
2023-03-11 07:22:08 +0000

    Update getip.sh
    removed backup host

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#
-# the original akamai site stopped working sometime around the end of 2021 
-# but i've decided to leave the original code in for posterity
-#
-#IPADDR=$(wget -q -O- http://whatismyip.akamai.com --no-check-certificate)
-#echo $IPADDR
-#
-IPADDR=$(dig +short myip.opendns.com @resolver1.opendns.com)
+IPADDR=$(wget -q -O- http://whatismyip.akamai.com --no-check-certificate)
 echo $IPADDR
+
