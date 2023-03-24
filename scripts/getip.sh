32a159fbbdd06707e21b58154c586f7875c2fbf1
walderf <76556660+walderf@users.noreply.github.com>
2023-03-24 07:41:35 -0500

    Update getip.sh

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -6,5 +6,12 @@
 #IPADDR=$(wget -q -O- https://whatismyip.akamai.com --no-check-certificate)
 #echo $IPADDR
 #
+# note: might not work with some VPN providers and/or public DNS providers, largely untested...
+#       ... meaning, i am not 100% sure or even 67% sure of the actual root of the issue(s).
+#
+# another note: if you experience network issues, try resolver2, 3, 4, etc... 
+#               ...worse case, you can't use the fancy DNS query below to find your IP...
+#               ... and need to use the original code above. 
+#
 IPADDR=$(dig +short myip.opendns.com @resolver1.opendns.com)
 echo $IPADDR
