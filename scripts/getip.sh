6880f1172926a4f7667bbd7ab8ab5638fef9975d
walderf <76556660+walderf@users.noreply.github.com>
2023-03-24 05:14:16 -0500

    Update getip.sh

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-IPADDR=$(wget -q -O- https://whatismyip.akamai.com --no-check-certificate)
+#
+# the original akamai site stopped working sometime around the end of 2021 
+# but i've decided to leave the original code in for posterity
+#
+#IPADDR=$(wget -q -O- https://whatismyip.akamai.com --no-check-certificate)
+#echo $IPADDR
+#
+IPADDR=$(dig +short myip.opendns.com @resolver1.opendns.com)
 echo $IPADDR
-
